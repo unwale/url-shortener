09663b5b0867a67646884be906c5fecfa59f0e64
Nikita Dubrovin <ndubrovin22@gmail.com>
2025-07-08 00:33:23 +0300

    Switch from map to database

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5"
 
 	"github.com/unwale/url-shortener/internal/api/handler"
 	"github.com/unwale/url-shortener/internal/domain/repository"
@@ -11,7 +13,14 @@ import (
 )
 
 func main() {
-	urlRepository := repository.NewURLRepository()
+	context := context.Background()
+	conn, err := pgx.Connect(context, "postgres://user:password@localhost:5432/url_shortener")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context)
+
+	urlRepository := repository.NewURLRepository(conn)
 	urlService := service.NewURLService(urlRepository)
 	urlHandler := handler.NewURLHandler(urlService)
 
--- internal/domain/model/url.go
+++ internal/domain/model/url.go
@@ -1,17 +1,8 @@
 package model
 
-type URL struct {
-	ID        string
-	Original  string
-	Shortened string
-	CreatedAt string
-	UpdatedAt string
-}
-
-type Error struct {
-	Message string `json:"message"`
-}
-
-func (e Error) Error() string {
-	return e.Message
+type Url struct {
+	OriginalUrl string
+	ShortUrl    string
+	CreatedAt   string
+	UpdatedAt   string
 }
