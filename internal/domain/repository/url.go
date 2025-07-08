1272416bf3cc73f2ae06044bb562d2b5e598ae5e
Nikita Dubrovin <ndubrovin22@gmail.com>
2025-07-08 18:46:00 +0300

    Count clicks
    - Extend URL repository interface
    - Increment click counter when resolving short URL

--- internal/domain/repository/url.go
+++ internal/domain/repository/url.go
@@ -13,6 +13,7 @@ import (
 type URLRepository interface {
 	CreateURL(ctx context.Context, url *db.CreateUrlParams) (*model.Url, error)
 	GetURLByShortened(ctx context.Context, shortened string) (*model.Url, error)
+	IncrementClickCount(ctx context.Context, shortened string) error
 }
 
 type urlRepository struct {
@@ -56,6 +57,14 @@ func (r *urlRepository) GetURLByShortened(ctx context.Context, shortened string)
 		CreatedAt:   url.CreatedAt.Time.Format(time.RFC3339),
 		UpdatedAt:   url.UpdatedAt.Time.Format(time.RFC3339),
 	}, nil
+}
+
+func (r *urlRepository) IncrementClickCount(ctx context.Context, shortened string) error {
+	_, err := r.querier.IncrementClickCount(ctx, shortened)
+	if err != nil {
+		return ErrURLNotFound
+	}
+	return nil
 }
 
 var (
