d4187829e6791edb48828b7cf18f3687900f1655
Nikita Dubrovin <ndubrovin22@gmail.com>
2025-07-08 18:32:26 +0300

    Add click_count migration, update database interface

--- db/query/url.sql
+++ db/query/url.sql
@@ -4,6 +4,12 @@ VALUES ($1, $2)
 RETURNING id, original_url, short_url, created_at, updated_at;
 
 -- name: GetUrlByShort :one
-SELECT id, original_url, short_url, created_at, updated_at
+SELECT id, original_url, short_url, click_count, created_at, updated_at
 FROM urls
 WHERE short_url = $1;
+
+-- name: IncrementClickCount :one
+UPDATE urls
+SET click_count = click_count + 1, updated_at = NOW()
+WHERE short_url = $1
+RETURNING id, original_url, short_url, click_count, created_at, updated_at;
